c4739c9262feeeba1bd18fbe3534244464013fed
liketech2007 <liketech2007@gmail.com>
2023-05-18 12:01:09 +0100

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,16 +23,16 @@ export default function RootLayout({
       <head>
       <script dangerouslySetInnerHTML={{
         __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-5PBF3DR');`}}></script>
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KGQ6KK9');`}}></script>
          <link rel="icon" type="image/x-icon" href={logo.src} /> 
       </head>
       <body className='bg-primary text-textP dark:bg-secondary dark:text-textS flex flex-col lg:justify-center lg:items-center md:justify-center md:items-center'>
         <noscript dangerouslySetInnerHTML={{
-       __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5PBF3DR"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+       __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGQ6KK9"
+       height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
       <ApolloProvider client={client}>
       <ContextTheme.Provider value={{theme,setTheme}}>
         <Cookie />
